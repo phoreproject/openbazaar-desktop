1dd6acf4942cd0260c9358332d3ee752a15955c6
Julian Meyer <julianmeyer2000@gmail.com>
2018-01-30 07:18:25 -0800

    Fix eslint conformance

--- js/utils/localServer.js
+++ js/utils/localServer.js
@@ -85,7 +85,11 @@ export default class LocalServer {
     }
 
     this._isRunning = true;
-    let serverStartArgs = ['start', '--runseparatewallet', `--daemonlocation=${this.serverPath + this.daemonFilename}`, ...commandLineArgs];
+    let serverStartArgs = ['start',
+      '--runseparatewallet',
+      `--daemonlocation=${this.serverPath + this.daemonFilename}`,
+      ...commandLineArgs,
+    ];
 
     // wire in our auth cookie
     if (global.authCookie) {
